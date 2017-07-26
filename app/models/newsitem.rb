f27ef56cca1572db7620b0654436883b8b0362db
Jan Lindblad <jlindbla@cisco.com>
2017-07-26 23:00:58 +0200

    Some initial interest score calculations

--- app/models/newsitem.rb
+++ app/models/newsitem.rb
@@ -6,4 +6,23 @@ class Newsitem < ApplicationRecord
   def to_s
     "Newsitem #{id} - #{name}"
   end
+
+  def get_interest_score
+  	puts 1
+  	user = 3 # TODO
+  	score = 0
+  	taglist = tags ? tags.split(" ") : []
+  	puts taglist
+  	taglist.each do |tag_name|
+  	  tag = Tag.find_by(name: tag_name)
+  	  if tag then
+        interest = UserInterest.find_by(id: user*1000+tag.id)
+  	    if interest then 
+  	  	  puts "Tag #{tag_name} adding score #{interest.weight.to_s}"
+  	  	  score += interest.weight 
+  	  	end
+  	  end
+  	end
+  	score
+  end
 end
