638167c8a652a8e11a6cd2d41b3b4a72d6f60802
Jan Lindblad <jlindbla@cisco.com>
2017-08-09 23:05:16 +0200

    Basic post_score and streak computations

--- app/controllers/newsitems_controller.rb
+++ app/controllers/newsitems_controller.rb
@@ -1,6 +1,8 @@
 class NewsitemsController < ApplicationController
   before_action :set_newsitem, only: [:show, :edit, :update, :destroy]
 
+  SCORE_NEW_POST = 5
+  SCORE_FIRST_POST = 25
 
   # GET /newsitems
   # GET /newsitems.json
@@ -32,6 +34,52 @@ class NewsitemsController < ApplicationController
     puts "Created by user #{current_user.id}"
     @newsitem.created_by = current_user.id
     @newsitem.liked_by @user
+
+    # New post, update streak counters
+    last_newsitem = Newsitem.find_by_sql [
+        "select created_at from newsitems where created_by=? order by created_at desc limit 1", current_user.id]
+    if last_newsitem.count >= 1 then 
+      last_post_date = Time.at(last_newsitem.first.created_at).to_date
+      today = Date.today
+
+      # Day streak
+      if last_post_date == today then
+        # Day streak preserved, already posted today
+      elsif last_post_date == today - 1 then
+        # Day streak preserved, this is the first post today
+        current_user.streak_d = current_user.streak_d ? current_user.streak_d + 1 : 1
+      else
+        # Day streak lost
+        current_user.streak_d = 0
+      end
+
+      # Week streak
+      if last_post_date.cweek == today.cweek and last_post_date.year == today.year then
+        # Week streak preserved, already posted this week
+      elsif last_post_date.cweek == today.cweek - 1 or (last_post_date.cweek >= 52 and today.cweek == 1) then
+        # FIXME: Check years also
+        # Week streak preserved, this is the first post this week
+        current_user.streak_w = current_user.streak_w ? current_user.streak_w + 1 : 1
+      else
+        # Week streak lost
+        current_user.streak_w = 0
+      end
+
+      # Month streak
+      if last_post_date.mon == today.mon and last_post_date.year == today.year then
+        # Month streak preserved, already posted this month
+      elsif last_post_date.mon == today.mon - 1 or (last_post_date.mon == 12 and today.mon == 1) then
+        # FIXME: Check years also
+        # Month streak preserved, this is the first post this month
+        current_user.streak_m = current_user.streak_m ? current_user.streak_m + 1 : 1
+      else
+        # Week streak lost
+        current_user.streak_m = 0
+      end
+
+    end
+    current_user.post_score = current_user.post_score ? current_user.post_score + SCORE_NEW_POST : SCORE_FIRST_POST
+    current_user.save
 
     respond_to do |format|
       if @newsitem.save
